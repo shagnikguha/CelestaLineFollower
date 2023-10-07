1c09f6d7a291798a36d02f87079703bf25d20f48
shagnikguha <125911642+shagnikguha@users.noreply.github.com>
2023-10-07 12:08:48 +0530

    Add files via upload

---
+++ LineFollower/irarray.ino
@@ -0,0 +1,89 @@
+/*
+0 -> black
+1 -> white
+*/
+
+//**********5 Channel IR Sensor Connection**********//
+int const ir1 = A0;
+int const ir2 = A1;
+int const ir3 = A2;
+int const ir4 = A3;
+int const ir5 = A4;
+//*************************************************//
+
+void setup() {
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(ir4, INPUT);
+  pinMode(ir5, INPUT);
+}
+
+void loop() {
+  //Reading Sensor Values
+  int s1 = digitalRead(ir1);  //Left Most Sensor
+  int s2 = digitalRead(ir2);  //Left Sensor
+  int s3 = digitalRead(ir3);  //Middle Sensor
+  int s4 = digitalRead(ir4);  //Right Sensor
+  int s5 = digitalRead(ir5);  //Right Most Sensor
+
+  //if only middle sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going forward with full speed 
+  }
+  
+  //if only left sensor detects black line
+  if((s1 == 1) && (s2 == 0) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed 
+  }
+  
+  //if only left most sensor detects black line
+  if((s1 == 0) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed
+  }
+
+  //if only right sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 0) && (s5 == 1))
+  {
+    //going left with full speed
+  }
+
+  //if only right most sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 0))
+  {
+    //going left with full speed 
+  }
+
+  //if middle and right sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 1))
+  {
+    //going left with full speed
+  }
+
+  //if middle and left sensor detects black line
+  if((s1 == 1) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed
+  }
+
+  //if middle, left and left most sensor detects black line
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed
+  }
+
+  //if middle, right and right most sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    //going left with full speed 
+  }
+
+  //if all sensors are on a black line
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    //stop
+  }
+}
